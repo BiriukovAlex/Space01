7c647f6b887f9d438e67c81fb64ecd77cc6a93c8
BiriukovAlex <115286143+biriukovalex@users.noreply.github.com>
2025-12-27 23:44:38 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import math
 import random
 import pygame
 
+
 #-----------------------------------------------------------------------------------------------------------------------
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 RES = WIDTH, HEIGHT = 800, 600
 FPS = 60
+FONT_SIZE=20
 
 #-----------------------------------------------------------------------------------------------------------------------
 pygame.init()
@@ -15,10 +17,29 @@ pygame.display.set_caption("Space 01")
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-
+font = pygame.font.Font(None, FONT_SIZE)
+running = True
 print ("Start")
 #-----------------------------------------------------------------------------------------------------------------------
 
+def text():
+    value = font.render("sd", True, pygame.Color('deeppink'))
+    window.blit(value, (0, 0))
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    window.fill('black')
+    text()
+
+    pygame.display.flip()
+    clock.tick(FPS)
+print("Exit")
+pygame.quit()
+sys.exit()
